de88426f8473ad085bcec9f2b057fa43260de87f
Lucas Sosa <lucassosa2019@hotmail.com>
2024-05-30 17:16:58 -0300

    cambios en la base de datos

--- database/casino.sql
+++ database/casino.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2024 a las 17:47:26
+-- Tiempo de generación: 30-05-2024 a las 22:11:08
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -40,43 +40,8 @@ CREATE TABLE `agentes` (
 --
 
 INSERT INTO `agentes` (`id_agente`, `nombre`, `saldo`, `email`, `activado`) VALUES
-(11, 'Lucas', 320.00, 'lucasgaturrososa@hotmail.com', 1),
-(12, 'Ale', 320.00, 'Alejandro@gmail.com', 1),
-(13, 'Manuel', 320000.00, 'manueliriart@gmail.com', 0);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cliente`
---
-
-CREATE TABLE `cliente` (
-  `id_cliente` int(11) NOT NULL,
-  `nombre_usuario` varchar(100) NOT NULL,
-  `saldo` double(10,2) NOT NULL,
-  `activado` tinyint(1) NOT NULL,
-  `id_agente` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `rol` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`id_usuario`, `email`, `password`, `rol`) VALUES
-(1, 'lucassosa2019@hotmail.com', '$2y$10$30sN3AyNuJhBMKXh19yDj.5J/9Xi3epHL2xQzZMbYXnEy4GT5skem', 'admin');
+(1, 'juan', 2000.00, 'juian@gmail.com', 1),
+(3, 'Lucas', 102000.00, 'LucasSosa2019@hotmail.com', 1);
 
 --
 -- Índices para tablas volcadas
@@ -89,19 +54,6 @@ ALTER TABLE `agentes`
   ADD PRIMARY KEY (`id_agente`);
 
 --
--- Indices de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id_cliente`),
-  ADD KEY `fk_cliente_agentes` (`id_agente`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -109,29 +61,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `agentes`
 --
 ALTER TABLE `agentes`
-  MODIFY `id_agente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD CONSTRAINT `fk_cliente_agentes` FOREIGN KEY (`id_cliente`) REFERENCES `agentes` (`id_agente`);
+  MODIFY `id_agente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
